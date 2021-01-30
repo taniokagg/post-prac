0841b02dc0fab431ccfd948297fdeb8188912038
tanioka <tanioka.mail@gmail.com>
2021-01-30 18:23:27 +0900

    形はできましたがAPIの導入方法が分かりません

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <input type="address" v-model="address">
+    <button @click="serch">住所自動入力</button>
+    <p>Address:{{}}</p>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import axios from "axios";
+export default {
+  methods: {
+    async serch() {
+    const item = await axios.get(`https://apis.postcode-jp.com/api/v4/U5NZRvRIZStqlmUptvesc9x2LP0wiOj11LKjSxW/postcode?allAddress
+`);
+    console.log(item);
+    }
+  },
+  data() {
+    return {
+      address: "",
+    };
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
