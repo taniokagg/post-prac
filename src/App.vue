4fa66e348cfe438cad2b53a7d858016afbbb9ccc
tanioka <tanioka.mail@gmail.com>
2021-02-06 21:16:34 +0900

    これでいけると思うのですがエラーが出てしまいます

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <input type="address" v-model="address">
     <button @click="serch">住所自動入力</button>
-    <p>Address:{{}}</p>
+    <p>Address:{{showAddress}}</p>
   </div>
 </template>
 
@@ -14,12 +14,13 @@ export default {
       address: "",
     };
   },
-  methods: {
-    async serch() {
-    const item = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/{{this.address}}?apikey=U5NZRvRIZStqlmUptvesc9x2LP0wiOj11LKjSxW`);
-    console.log(item);
+    methods: {
+      async serch() {
+        const item = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.address}?apikey=U5NZRvRIZStqlmUptvesc9x2LP0wiOj11LKjSxW`);
+        return showAddress = item.data[0].allAdress;
+      }
     }
   }
-}
 </script>
+
 
